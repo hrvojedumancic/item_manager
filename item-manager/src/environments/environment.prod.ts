4faa1f3cf0237fa3fca001ecc10b66ae8e2a257a
Hrvoje Dumancic <hdumancic1@gmail.com>
2021-02-14 11:39:54 +0100

    Added the same firebase project configuration to the production environment file so that I have the same database when deploying the application

--- item-manager/src/environments/environment.prod.ts
+++ item-manager/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBqph4qlXCptA3GkPjfikNCDi2fPfyBe8M",
+    authDomain: "item-manager-d1cd7.firebaseapp.com",
+    projectId: "item-manager-d1cd7",
+    storageBucket: "item-manager-d1cd7.appspot.com",
+    messagingSenderId: "773497700307",
+    appId: "1:773497700307:web:78be9e307e7556719ab926",
+    measurementId: "G-47QVTE2CVN"
+  }
 };
